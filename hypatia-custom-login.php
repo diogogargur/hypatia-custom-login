5e98d7da4d09275b0b5aa711c783f8df6d3feafa
Diogo Gargur <diogogargur@gmail.com>
2020-03-10 02:34:15 -0300

    Adição dos comentários com as informações do plugin

--- hypatia-custom-login.php
+++ hypatia-custom-login.php
@@ -1,5 +1,15 @@
-<!-- 
-
-    Pluign Name: Hypatia Custom Login
-
- -->
+<?php
+/**
+ * @package Hypatia Custom Login
+ */
+/*
+Plugin Name: Hypatia Custom Login
+Plugin URI: https://github.com/diogogargur/hypatia-custom-login
+Description: Plugin that changes the main aspect and links from the admin login page.
+Version: 1.0.0
+Author: Hypatia Creative Studio
+Author URI: https://github.com/diogogargur
+License: GPLv2 or later
+Text Domain: hypatia-custom-login
+*/
+?>
